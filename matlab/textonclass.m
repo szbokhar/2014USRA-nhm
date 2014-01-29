b0345a167b0e720169b4d051da67bde39dfe74bf
Syed Zahir Bokhari <syedzahirbokhari@gmail.com>
2014-01-29 02:27:20 -0500

    Add variable to easilt switch on/off filters. Fill in line for SVM Soft Margin Quadratic Programming.

--- matlab/textonclass.m
+++ matlab/textonclass.m
@@ -6,10 +6,10 @@ function newimg = texton(fname, box)
     lap = fspecial('laplacian');
     grad_x = fspecial('sobel')';
     grad_y = fspecial('sobel');
-    k_g1 = fspecial('gaussian', 23, 1);
-    k_g2 = fspecial('gaussian', 23, 2);
-    k_g3 = fspecial('gaussian', 23, 3);
-    k_g4 = fspecial('gaussian', 23, 4);
+    k_g1 = fspecial('gaussian', 9, 0.5);
+    k_g2 = fspecial('gaussian', 9, 1);
+    k_g3 = fspecial('gaussian', 9, 1.5);
+    k_g4 = fspecial('gaussian', 9, 2);
     k_g1L = imfilter(k_g1,lap);
     k_g2L = imfilter(k_g2,lap);
     k_g3L = imfilter(k_g3,lap);
@@ -18,15 +18,18 @@ function newimg = texton(fname, box)
     k_g4X = imfilter(k_g4,grad_x);
     k_g3Y = imfilter(k_g3,grad_y);
     k_g4Y = imfilter(k_g4,grad_y);
-    lab = RGB2Lab(img);
+    cvt = makecform('srgb2lab');
+    lab = applycform(img, cvt);
+    lab = double(lab);
 
     gfilters = {k_g1, k_g2, k_g3};
     lfilters = {k_g1L, k_g2L, k_g3L, k_g4L};
     xyfilters = {k_g3X, k_g4X, k_g3Y, k_g4Y};
+    fcount = 9;
 
     % Setup storage to response to filters
     count = 1;
-    responses{18} = ones(size(img(:,:,1)));
+    responses{fcount+1} = ones(size(img(:,:,1)));
 
     % Run filters
     for j=1:3
@@ -38,6 +41,7 @@ function newimg = texton(fname, box)
             count = count+1;
         end
     end
+    %{
     for i=1:4
         neww = imfilter(lab(:,:,1),lfilters{i});
         responses{count} = neww;
@@ -48,11 +52,12 @@ function newimg = texton(fname, box)
         responses{count} = neww;
         count = count+1;
     end
+    %}
 
     % Store responses in HxWx18 dimentional array
     stack(:,:,1) = responses{1};
 
-    for i = 2:18
+    for i = 2:(fcount+1)
         stack(:,:,i) = responses{i};
     end
 
@@ -66,7 +71,7 @@ function newimg = texton(fname, box)
         bx = round(clicks(i,1));
         by = round(clicks(i,2));
         bsample = stack((by-box):(by+box),(bx-box):(bx+box),:);
-        temp = reshape(bsample,(box*2+1)^2,18);
+        temp = reshape(bsample,(box*2+1)^2,fcount+1);
         bs = [bs; temp];
         disp('bclick');
     end
@@ -79,18 +84,18 @@ function newimg = texton(fname, box)
         fx = round(clicks(i,1));
         fy = round(clicks(i,2));
         fsample = stack((fy-box):(fy+box),(fx-box):(fx+box),:);
-        temp = reshape(fsample,(box*2+1)^2,18);
+        temp = reshape(fsample,(box*2+1)^2,fcount+1);
         fs = [fs; temp];
         disp('fclick');
     end
 
-    bs(:,19) = 0;
-    fs(:,19) = 1;
+    bs(:,fcount+2) = 0;
+    fs(:,fcount+2) = 1;
     data = [bs;fs]
 
     % Train svm
     disp('Training SVM')
-    machine = svmtrain(data(:,1:18), data(:,19));
+    machine = svmtrain(data(:,1:(fcount+1)), data(:,fcount+2), 'method','QP');
 
     % Classify all points
     [h w] = size(img(:,:,1));
@@ -98,7 +103,7 @@ function newimg = texton(fname, box)
 
     for y = 1:h
         for x = 1:w
-            sample = reshape(stack(y,x,:),1,18);
+            sample = reshape(stack(y,x,:),1,fcount+1);
             class = svmclassify(machine,sample);
             newimg(y,x) = class;
         end
