fbfd438cbd9201da6ac0a22bed1139c3db96c49f
Syed Zahir Bokhari <syedzahirbokhari@gmail.com>
2014-12-03 11:21:41 -0500

    Add LICENSE file and headers

--- python/witnesscam_gui/Hints.py
+++ python/witnesscam_gui/Hints.py
@@ -1,3 +1,22 @@
+# Hint text constants
+#
+# Technology for Nature. British Natural History Museum insect specimen
+# digitization project.
+#
+# Copyright (C) 2014    Syed Zahir Bokhari, Prof. Michael Terry
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+
 HINT_LOADFILE = """Load a tray scan image by draggging a file here or using the file menu"""
 HINT_TRAYAREA_1 = """Click on the top left corner of the tray in the scanned image in the live camera view"""
 HINT_TRAYAREA_234 = "Now click on the next corner clockwise"
